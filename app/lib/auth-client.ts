060283f1b87662e49b78385a24c6ae8e1b83c032
Rachhen <rachhen.it@outlook.com>
2024-12-13 15:59:09 +0700

    update auth base url

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 
+const VERCEL_URL = import.meta.env.VERCEL_URL;
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:5173", // the base url of your auth server
+  baseURL: VERCEL_URL ? `https://${VERCEL_URL}` : "http://localhost:5173", // the base url of your auth server
 });
